17d0d6ca9311899ec9703cfb71e10f88e538badd
seonpilKim <ksp970306@gmail.com>
2021-12-20 22:33:06 +0900

    Fix: cors allow-origin 수정

--- src/main/java/project/mbti/config/WebConfig.java
+++ src/main/java/project/mbti/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigin + "/**")
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
