7d04988b82a3f094bb73afc1ff3c5d6494e74920
seonpilKim <ksp970306@gmail.com>
2021-12-20 20:26:29 +0900

    Etc: Swagger-ui 설정 제거, 허용 origin 하나로 제한(cors)

--- src/main/java/project/mbti/config/SwaggerConfig.java
+++ src/main/java/project/mbti/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@Configuration
+//@Configuration
 public class SwaggerConfig {
 
     @Bean
--- src/main/java/project/mbti/config/WebConfig.java
+++ src/main/java/project/mbti/config/WebConfig.java
@@ -1,5 +1,6 @@
 package project.mbti.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("allow-origin")
+    String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins(allowedOrigin + "/**")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
