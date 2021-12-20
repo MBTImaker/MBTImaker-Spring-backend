c616fec0ecd89d95b010f2e83dc8fd6bcccad8a1
seonpilKim <ksp970306@gmail.com>
2021-12-20 18:36:46 +0900

    Feat: HomeController 추가
    - for Health Check

---
+++ src/main/java/project/mbti/HomeController.java
@@ -0,0 +1,13 @@
+package project.mbti;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "success";
+    }
+}
